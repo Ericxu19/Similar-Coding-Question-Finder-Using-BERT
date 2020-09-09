eb91d22ed88120d0e39acefa08f0cffb31001354
eric <xihh01@gmail.com>
2020-09-09 15:29:42 -0700

    links

--- main.py
+++ main.py
@@ -2,14 +2,16 @@ from sentence_transformers import SentenceTransformer, util
 import numpy as np
 import csv 
 import argparse
+import json
 parser = argparse.ArgumentParser()
 parser.add_argument("question", help="enter the question here")
 
 args = parser.parse_args()
 queries = [args.question]
 
-embedder = SentenceTransformer('model')
-#embedder = SentenceTransformer('distilbert-base-nli-stsb-mean-tokens')
+embedder = SentenceTransformer('Pb4e2')
+with open('reverse.json', 'r') as f:
+    reverse = json.load(f)
 
 # Corpus with example sentences
 with open('Qcollection.csv', newline = '', encoding='utf-8') as f:
@@ -19,11 +21,11 @@ corpus = data[0]
 corpus_embeddings = embedder.encode(corpus, convert_to_tensor=True)
 
 # Query sentences:
-"""
+
 queries = ['You are given coins of different denominations and a total amount of money amount. Write a function to compute the fewest number of coins that you need to make up that amount. If that amount of money cannot be made up by any combination of the coins, return -1.', 
 'You are given pigs of different denominations and a total amount of pigs amount. Write a function to compute the fewest number of pigs that you need to make up that amount. If that amount of pigs cannot be made up by any combination of the pigs, return -1', 
 'Umbrellas are available in different sizes that are each able to shelter a certain number of people. Given the number of people needing shelter and a list of umbrella sizes, determine the minimum number of umbrellas necessary to cover exactly the number of people given, and no more. If there is no combination that covers exactly that number of people, return -1']
-"""
+
 
 # Find the closest 5 sentences of the corpus for each query sentence based on cosine similarity
 top_k = 5
@@ -40,5 +42,6 @@ for query in queries:
     print("\nTop 5 most similar questionss on Leetcode:")
 
     for idx in top_results[0:top_k]:
+        print("https://leetcode.com/problems/"+ reverse[corpus[idx]][0])
         print(corpus[idx].strip(), "(Score: %.4f)" % (cos_scores[idx]))
-        print("\n======================\n")
+        print("===========================\n")
