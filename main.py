a14b6c47a0eb9f5747f315677703e1890a206a26
xihh01@gmail.com <xihh01@gmail.com>
2020-09-15 23:55:16 -0700

    reverse

--- main.py
+++ main.py
@@ -9,12 +9,16 @@ parser.add_argument("question", help="enter the question here")
 args = parser.parse_args()
 queries = [args.question]
 
-embedder = SentenceTransformer('Pb4e2')
+embedder = SentenceTransformer('pT6b8e4roberta-large-nli-stsb-mean-tokens-2020-09-12_23-37-54')
+
+#for name, param in embedder.named_parameters():
+#    if param.requires_grad:print(name)
+
 with open('reverse.json', 'r') as f:
     reverse = json.load(f)
 
 # Corpus with example sentences
-with open('Qcollection.csv', newline = '', encoding='utf-8') as f:
+with open('pQcollection.csv', newline = '', encoding='utf-8') as f:
     reader = csv.reader(f)
     data = list(reader)
 corpus = data[0]
@@ -28,7 +32,7 @@ queries = ['You are given coins of different denominations and a total amount of
 
 
 # Find the closest 5 sentences of the corpus for each query sentence based on cosine similarity
-top_k = 5
+top_k = 8
 for query in queries:
     query_embedding = embedder.encode(query, convert_to_tensor=True)
     cos_scores = util.pytorch_cos_sim(query_embedding, corpus_embeddings)[0]
@@ -37,11 +41,22 @@ for query in queries:
     #We use np.argpartition, to only partially sort the top_k results
     top_results = np.argpartition(-cos_scores, range(top_k))[0:top_k]
 
-    print("\n\n======================\n\n")
+    print("\n\n=================================")
     print("Query:", query)
     print("\nTop 5 most similar questionss on Leetcode:")
-
+    tags ={}
     for idx in top_results[0:top_k]:
+        if reverse[corpus[idx]][2] != []:
+            for tag in reverse[corpus[idx]][2]:
+                if tag in tags:
+                    tags[tag] +=1
+                else:
+                    tags[tag] = 1
         print("https://leetcode.com/problems/"+ reverse[corpus[idx]][0])
         print(corpus[idx].strip(), "(Score: %.4f)" % (cos_scores[idx]))
-        print("===========================\n")
+        print("===========================")
+
+    print("++++++++++++Tags++++++++++++")
+    for key, value in sorted(tags.items(), key=lambda item: item[1], reverse=True):
+        print("%s: %s" % (key, value))
+    print("++++++++++++END+++++++++++++\n")
